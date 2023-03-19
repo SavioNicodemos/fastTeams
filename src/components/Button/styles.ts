c449d54b5294b913737e3e315b84ececcd28d5a6
NicodemosSantos <111062089+NicodemosSantos@users.noreply.github.com>
2023-03-19 03:56:14 +0000

    refactor: use theme only 1 time in styles.ts

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,11 +1,11 @@
-import { TouchableOpacity } from 'react-native';
-import styled from 'styled-components/native';
+import { TouchableOpacity } from "react-native";
+import styled, { css } from "styled-components/native";
 
-export type ButtonTypeStyleProps = 'PRIMARY' | 'SECONDARY';
+export type ButtonTypeStyleProps = "PRIMARY" | "SECONDARY";
 
 type Props = {
   type: ButtonTypeStyleProps;
-}
+};
 
 export const Container = styled(TouchableOpacity)<Props>`
   flex: 1;
@@ -13,7 +13,8 @@ export const Container = styled(TouchableOpacity)<Props>`
   min-height: 56px;
   max-height: 56px;
 
-  background-color: ${({ theme, type }) => type === 'PRIMARY' ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
+  background-color: ${({ theme, type }) =>
+    type === "PRIMARY" ? theme.COLORS.GREEN_700 : theme.COLORS.RED_DARK};
 
   border-radius: 6px;
   justify-content: center;
@@ -21,7 +22,9 @@ export const Container = styled(TouchableOpacity)<Props>`
 `;
 
 export const Title = styled.Text`
-  font-size: ${({theme}) => theme.FONT_SIZE.MD}px;
-  color: ${({theme}) => theme.COLORS.WHITE};
-  font-family: ${({theme}) => theme.FONT_FAMILY.BOLD};
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    color: ${theme.COLORS.WHITE};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+  `};
 `;
