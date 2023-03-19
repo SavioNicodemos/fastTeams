24a9929e9387c6a5fec595fcd22362b2545eb320
NicodemosSantos <111062089+NicodemosSantos@users.noreply.github.com>
2023-03-19 02:40:51 +0000

    Added EmptyList Component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,10 +4,11 @@ import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { GroupCard } from '@components/GroupCard';
 import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
 import { Container } from './styles'
 
 export const Groups: React.FC = () => {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Galera do bem']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -23,6 +24,10 @@ export const Groups: React.FC = () => {
           <GroupCard 
             title={item} 
           />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
     </Container>
