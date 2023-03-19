295c5a8c1b1e6cca967afe547b30307963b70510
NicodemosSantos <111062089+NicodemosSantos@users.noreply.github.com>
2023-03-19 02:33:12 +0000

    Config flatlist for groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
 import { GroupCard } from '@components/GroupCard';
 import { Highlight } from '@components/Highlight';
 import { Container } from './styles'
 
 export const Groups: React.FC = () => {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Galera do bem']);
+
   return (
     <Container>
       <Header />
@@ -11,7 +16,15 @@ export const Groups: React.FC = () => {
         title="Turmas"
         subtitle="jogue com a sua turma"
       />
-      <GroupCard title="Galera do Ignite" />
+       <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+      />
     </Container>
   );
 }
