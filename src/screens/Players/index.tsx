ede712bb5a79e5c896ba01e0cc4a8699360a8170
NicodemosSantos <111062089+NicodemosSantos@users.noreply.github.com>
2023-03-20 14:30:10 +0000

    Fixed input with icon inside

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,23 +3,25 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 
 export function Players() {
   return (
     <Container>
       <Header showBackButton />
 
-      <Highlight 
+      <Highlight
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
 
-      <Input
-        placeholder="Nome da pessoa"
-        autoCorrect={false}
-      />
-      <ButtonIcon icon="add"/>
+      <Form>
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+        <ButtonIcon icon="add" />
+      </Form>
     </Container>
   )
 }
